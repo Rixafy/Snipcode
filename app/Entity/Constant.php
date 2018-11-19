31e7fd249982272a02a3dceb9cb5cd008503fee1
Rick Strafy <rixafy@gmail.com>
2018-11-19 19:45:37 +0100

    Created entity capable to store basic site config

---
+++ app/Entity/Constant.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="constant")
+ */
+class Constant
+{
+    use UniqueTrait;
+
+    /**
+     * @ORM\Column(type="string", unique=true, length=31)
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=31)
+     * @var string
+     */
+    private $value;
+
+    /**
+     * Constant constructor.
+     * @param string $name
+     * @param string $value
+     */
+    public function __construct(string $name, string $value)
+    {
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue(string $value): void
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param int $value
+     */
+    public function setIntValue(int $value): void
+    {
+        $this->value = strval($value);
+    }
+
+    /**
+     * @param bool $value
+     */
+    public function setBoolValue(bool $value): void
+    {
+        $this->value = strval($value);
+    }
+
+    /**
+     * @return bool
+     */
+    public function getBoolValue(): bool
+    {
+        return boolval($this->value);
+    }
+
+    /**
+     * @return int
+     */
+    public function getIntValue(): int
+    {
+        return intval($this->value);
+    }
+}
---
+++ app/Entity/Traits/UniqueTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entity;
+
+trait UniqueTrait
+{
+    /**
+     * @var \Ramsey\Uuid\UuidInterface
+     *
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    protected $id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}
