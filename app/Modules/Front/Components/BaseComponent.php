4e9281658f4457c6e56fdb48196d5dd341c5b3a7
Rick Strafy <rixafy@gmail.com>
2018-11-21 19:19:07 +0100

    Created abstract class BaseComponent

---
+++ app/Modules/Front/Components/BaseComponent.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Component;
+
+use Nette\Application\UI\Control;
+
+abstract class BaseComponent extends Control
+{
+    /** @var array[] */
+    protected $onChange = [];
+
+    /** @var array[] */
+    protected $onSuccess = [];
+
+    /** @var array[] */
+    protected $onFail = [];
+
+    public function render()
+    {
+        $template = $this->template;
+
+        $template->setFile('../app/Modules/Front/Templates/@Components/'.$this->getName().'.latte');
+
+        $template->render();
+    }
+
+    /**
+     * @param callable $action
+     * @return $this
+     */
+    public function addOnChange(callable $action): BaseComponent
+    {
+        $this->onChange[] = $action;
+
+        return $this;
+    }
+
+    /**
+     * @param callable $action
+     * @return $this
+     */
+    public function addOnSuccess(callable $action): BaseComponent
+    {
+        $this->onSuccess[] = $action;
+
+        return $this;
+    }
+
+    /**
+     * @param callable $action
+     * @return $this
+     */
+    public function addOnFail(callable $action): BaseComponent
+    {
+        $this->onFail[] = $action;
+
+        return $this;
+    }
+
+    public function onChange(): void
+    {
+        foreach ($this->onChange as $action) {
+            $action();
+        }
+    }
+
+    public function onSuccess(): void
+    {
+        foreach ($this->onSuccess as $action) {
+            $action();
+        }
+    }
+
+    public function onFail(): void
+    {
+        foreach ($this->onFail as $action) {
+            $action();
+        }
+    }
+}
