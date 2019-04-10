ba5f5428ca1a1d89f0363f6227187c2816ea2f86
Rixafy <rixafy@gmail.com>
2019-04-10 17:45:30 +0200

    Fix code style in Snippet

--- app/Model/Snippet/Snippet.php
+++ app/Model/Snippet/Snippet.php
@@ -15,81 +15,81 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity
  * @ORM\Table(name="snippet", indexes={
- *     @ORM\Index(name="search_index", columns={"slug"}),
- *     @ORM\Index(name="updated_order", columns={"updated_at"}),
- *     @ORM\Index(name="created_order", columns={"created_at"})
+ *	 @ORM\Index(name="search_index", columns={"slug"}),
+ *	 @ORM\Index(name="updated_order", columns={"updated_at"}),
+ *	 @ORM\Index(name="created_order", columns={"created_at"})
  * })
  * @ORM\HasLifecycleCallbacks
  */
 class Snippet
 {
-    use UniqueTrait;
-    use DateTimeTrait;
+	use UniqueTrait;
+	use DateTimeTrait;
 
-    /**
-     * @ORM\Column(type="string", nullable=true)
-     * @var string
-     */
-    private $title;
+	/**
+	 * @ORM\Column(type="string", nullable=true)
+	 * @var string
+	 */
+	private $title;
 
-    /**
-     * @ORM\Column(type="string", options={"collation":"utf8_bin"})
-     * @var string
-     */
-    private $slug;
+	/**
+	 * @ORM\Column(type="string", options={"collation":"utf8_bin"})
+	 * @var string
+	 */
+	private $slug;
 
-    /**
-     * @ORM\Column(type="integer")
-     * @var int
-     */
-    private $slug_helper;
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var int
+	 */
+	private $slug_helper;
 
-    /**
-     * @ORM\Column(type="text")
-     * @var string
-     */
-    private $payload;
+	/**
+	 * @ORM\Column(type="text")
+	 * @var string
+	 */
+	private $payload;
 
-    /**
-     * @ORM\Column(type="integer")
-     * @var int
-     */
-    private $views = 0;
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var int
+	 */
+	private $views = 0;
 
-    /**
-     * Many Snippets have One Session
-     * @ORM\ManyToOne(targetEntity="\App\Entity\Session", inversedBy="snippet", cascade={"persist"})
-     * @var Session
-     */
-    private $author_session;
+	/**
+	 * Many Snippets have One Session
+	 * @ORM\ManyToOne(targetEntity="\App\Entity\Session", inversedBy="snippet", cascade={"persist"})
+	 * @var Session
+	 */
+	private $author_session;
 
-    /**
-     * Many Snippets have One IpAddress
-     * @ORM\ManyToOne(targetEntity="\App\Entity\IpAddress", inversedBy="snippet", cascade={"persist"})
-     * @var IpAddress
-     */
-    private $author_ip_address;
+	/**
+	 * Many Snippets have One IpAddress
+	 * @ORM\ManyToOne(targetEntity="\App\Entity\IpAddress", inversedBy="snippet", cascade={"persist"})
+	 * @var IpAddress
+	 */
+	private $author_ip_address;
 
-    /**
-     * Many Snippets have One Syntax
-     * @ORM\ManyToOne(targetEntity="\App\Entity\Syntax", inversedBy="snippet", cascade={"persist"})
-     * @var Syntax
-     */
-    private $syntax;
+	/**
+	 * Many Snippets have One Syntax
+	 * @ORM\ManyToOne(targetEntity="\App\Entity\Syntax", inversedBy="snippet", cascade={"persist"})
+	 * @var Syntax
+	 */
+	private $syntax;
 
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     * @var DateTime
-     */
-    private $expire_at;
+	/**
+	 * @ORM\Column(type="datetime", nullable=true)
+	 * @var DateTime
+	 */
+	private $expire_at;
 
-    public function __construct(SnippetData $snippetData)
-    {
-        $this->edit($snippetData);
-        $this->author_session->addSnippet($this);
-    }
+	public function __construct(SnippetData $snippetData)
+	{
+		$this->edit($snippetData);
+		$this->author_session->addSnippet($this);
+	}
 
-    public function edit(SnippetData $snippetData): void
+	public function edit(SnippetData $snippetData): void
 	{
 		$this->title = $snippetData->title;
 		$this->payload = $snippetData->payload;
@@ -99,54 +99,54 @@ class Snippet
 		$this->expire_at = $snippetData->expireAt;
 	}
 
-    public function getSlug(): string
-    {
-        return $this->slug;
-    }
+	public function getSlug(): string
+	{
+		return $this->slug;
+	}
 
-    public function createSlug(int $slugHelper, string $slug): void
-    {
-    	$this->slug_helper = $slugHelper;
-        $this->slug = $slug;
-    }
+	public function createSlug(int $slugHelper, string $slug): void
+	{
+		$this->slug_helper = $slugHelper;
+		$this->slug = $slug;
+	}
 
-    public function getPayload(): string
-    {
-        return $this->payload;
-    }
+	public function getPayload(): string
+	{
+		return $this->payload;
+	}
 
-    public function getAuthorSession(): Session
-    {
-        return $this->author_session;
-    }
+	public function getAuthorSession(): Session
+	{
+		return $this->author_session;
+	}
 
-    public function getAuthorIpAddress(): IpAddress
-    {
-        return $this->author_ip_address;
-    }
+	public function getAuthorIpAddress(): IpAddress
+	{
+		return $this->author_ip_address;
+	}
 
-    public function getSyntax(): Syntax
-    {
-        return $this->syntax;
-    }
+	public function getSyntax(): Syntax
+	{
+		return $this->syntax;
+	}
 
-    public function getSlugHelper(): int
-    {
-        return $this->slug_helper;
-    }
+	public function getSlugHelper(): int
+	{
+		return $this->slug_helper;
+	}
 
-    public function getTitle(): string
-    {
-        return $this->title == null ? 'Snippet #'.$this->getSlug() : $this->title;
-    }
+	public function getTitle(): string
+	{
+		return $this->title == null ? 'Snippet #'.$this->getSlug() : $this->title;
+	}
 
-    public function getViews(): int
-    {
-        return $this->views;
-    }
+	public function getViews(): int
+	{
+		return $this->views;
+	}
 
-    public function addView(): void
-    {
-        $this->views++;
-    }
+	public function addView(): void
+	{
+		$this->views++;
+	}
 }
