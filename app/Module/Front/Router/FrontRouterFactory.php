68cc9a089dddf85eb96ef0d3b3dd6e30fad603ae
Rick Strafy <rixafy@gmail.com>
2019-04-11 09:42:24 +0200

    Move router to front module

---
+++ app/Module/Front/Router/FrontRouterFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Module\Front\Router;
+
+use Nette;
+use Nette\Application\Routers\RouteList;
+
+final class FrontRouterFactory
+{
+	use Nette\StaticClass;
+
+	/**
+	 * @return Nette\Application\IRouter
+	 */
+	public static function createRouter()
+	{
+		$router = new RouteList;
+
+		$router->addRoute('<slug>', 'Snippet:default');
+		$router->addRoute('<slug>', 'Snippet:default');
+		$router->addRoute('raw/<slug>', 'Snippet:raw');
+		$router->addRoute('fork/<forkId>', 'Homepage:default');
+		$router->addRoute('', 'Homepage:default');
+
+		return $router;
+	}
+}
